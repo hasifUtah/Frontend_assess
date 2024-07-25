d41cdd981771bae9bbfcac7997bf07d2c9cad04f
hasifUtah <85155454+hasifUtah@users.noreply.github.com>
2024-07-25 15:09:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).send('Sorry, we cannot find that!');
+});
 
 // Start the server
 app.listen(port, () => {
